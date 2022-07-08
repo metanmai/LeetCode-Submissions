8e7ae80ec41a31bdec99ec064e72a30d2a90906b
metanmai <103983719+metanmai@users.noreply.github.com>
2022-07-08 20:23:54 +0530

    Time: 77 ms (5.32%), Space: 14.4 MB (6.88%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,3 +1,20 @@
+# Using Dynamic Programming
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return factorial(m + n - 2) // factorial(m - 1) // factorial(n - 1)
+        d = {}
+        
+        def paths(m, n):
+            f = frozenset([m, n])
+            
+            if f in d:
+                return d[f]
+            
+            elif m == 1 or n == 1:
+                return 1
+            
+            else:
+                d[f] = paths(m - 1, n) + paths(m, n - 1)
+                return d[f]
+        
+        return paths(m, n)
