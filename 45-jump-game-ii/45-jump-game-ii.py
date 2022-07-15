7c40177de612f0ae1ec2ab1ccedc20e34014d7b8
metanmai <103983719+metanmai@users.noreply.github.com>
2022-07-15 14:02:37 +0530

    Time: 1248 ms (32.02%), Space: 20.3 MB (7.81%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        
+        def jumpp(x):
+            if x==0:
+                return 0
+            for i in range(x, -1, -1):
+                if i + nums[i] >= x:
+                    maxJump = i
+        
+            return 1 + jumpp(maxJump)
+        
+        return jumpp(len(nums) - 1)
