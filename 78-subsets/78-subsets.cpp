8f66b3878091bc567a77f9d86cd5b16e522e7490
metanmai <103983719+metanmai@users.noreply.github.com>
2022-07-24 18:13:49 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (56.19%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -3,20 +3,20 @@ public:
     vector<int> subset;
     vector<vector<int>> ans;
     
-    void generate_subsets(int start, int len, vector<int> nums)
+    void generate_subsets(int start, vector<int> nums)
     {
-        for(int i = start; i < len; i++)
+        for(int i = start; i < nums.size(); i++)
         {
             subset.push_back(nums[i]);
             ans.push_back(subset);
-            generate_subsets(i + 1, len, nums);
+            generate_subsets(i + 1, nums);
             subset.pop_back();
         }
     }
     
     vector<vector<int>> subsets(vector<int>& nums) {
         ans.push_back(subset);
-        generate_subsets(0, nums.size(), nums);
+        generate_subsets(0, nums);
         return ans;
     }
 };
