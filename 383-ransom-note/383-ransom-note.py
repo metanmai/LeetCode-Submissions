c7e8cdb2612fbea3bf1b2330ee65b07375d85be4
metanmai <103983719+metanmai@users.noreply.github.com>
2022-08-26 01:13:29 +0530

    Time: 78 ms (67.01%), Space: 14.2 MB (53.79%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,15 +1,16 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        mag_freq, ransom_freq = defaultdict(lambda : 0), defaultdict(lambda : 0)
+        mag_freq = defaultdict(lambda : 0)
         
         for i in magazine:
             mag_freq[i] += 1
         
         for i in ransomNote:
-            ransom_freq[i] += 1
-        
-        for i in ransom_freq:
-            if i not in mag_freq or mag_freq[i] < ransom_freq[i]:
+            if mag_freq[i] == 0:
                 return False
+            
+            else:
+                mag_freq[i] -= 1
         
         return True
+                
