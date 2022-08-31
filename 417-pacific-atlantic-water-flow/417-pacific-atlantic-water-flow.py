1f80d75a1b5b46a81fbc5ecb17db95f10a26cde7
metanmai <103983719+metanmai@users.noreply.github.com>
2022-08-31 15:37:06 +0530

    Time: 4292 ms (5.01%), Space: 15.5 MB (78.42%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -3,20 +3,19 @@ class Solution:
         answer = []
         r, c = len(heights), len(heights[0])
 
-        def canFlowPacific(x1, y1, x2, y2, visited):
-            if (x2, y2) in visited:
-                return False
-            
-            if x2 < 0 or y2 < 0:
+        def canFlowPacific(x1, y1, x2, y2, visited):       
+            if x2 < 0 or y2 < 0 :
                 return True
             
-            elif x2 >= r or y2 >= c:
-                return False
-            
-            elif heights[x1][y1] < heights[x2][y2]:
+            elif ((x2, y2) in visited or 
+                  x2 >= r or y2 >= c or 
+                  heights[x1][y1] < heights[x2][y2]):
                 return False
             
             visited.add((x2, y2))
+            if [x2, y2] in answer:
+                return True
+            
             return (canFlowPacific(x2, y2, x2 - 1, y2, visited) or
                     canFlowPacific(x2, y2, x2, y2 - 1, visited) or
                     canFlowPacific(x2, y2, x2 + 1, y2, visited) or
@@ -24,19 +23,18 @@ class Solution:
                     
                 
         def canFlowAtlantic(x1, y1, x2, y2, visited):
-            if (x2, y2) in visited:
-                return False
-            
             if x2 >= r or y2 >= c:
                 return True
             
-            elif x2 < 0 or y2 < 0:
-                return False
-            
-            elif heights[x1][y1] < heights[x2][y2]:
+            elif ((x2, y2) in visited or 
+                  x2 < 0 or y2 < 0 or 
+                  heights[x1][y1] < heights[x2][y2]):
                 return False
             
             visited.add((x2, y2))
+            if [x2, y2] in answer:
+                return True
+            
             return (canFlowAtlantic(x2, y2, x2 - 1, y2, visited) or
                     canFlowAtlantic(x2, y2, x2, y2 - 1, visited) or
                     canFlowAtlantic(x2, y2, x2 + 1, y2, visited) or
