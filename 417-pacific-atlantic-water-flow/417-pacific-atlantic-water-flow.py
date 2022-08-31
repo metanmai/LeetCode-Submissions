fd6785c26de54471b637b2835e69432f3874e7c6
metanmai <103983719+metanmai@users.noreply.github.com>
2022-08-31 15:08:46 +0530

    Time: 1954 ms (8.50%), Space: 15.5 MB (95.25%) - LeetHub

---
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -0,0 +1,51 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        answer = []
+        r, c = len(heights), len(heights[0])
+
+        def canFlowPacific(x1, y1, x2, y2, visited):
+            if (x2, y2) in visited:
+                return False
+            
+            if x2 < 0 or y2 < 0:
+                return True
+            
+            elif x2 >= r or y2 >= c:
+                return False
+            
+            elif heights[x1][y1] < heights[x2][y2]:
+                return False
+            
+            visited.add((x2, y2))
+            return (canFlowPacific(x2, y2, x2 - 1, y2, visited) or
+                    canFlowPacific(x2, y2, x2, y2 - 1, visited) or
+                    canFlowPacific(x2, y2, x2 + 1, y2, visited) or
+                    canFlowPacific(x2, y2, x2, y2 + 1, visited))
+                    
+                
+        def canFlowAtlantic(x1, y1, x2, y2, visited):
+            if (x2, y2) in visited:
+                return False
+            
+            if x2 >= r or y2 >= c:
+                return True
+            
+            elif x2 < 0 or y2 < 0:
+                return False
+            
+            elif heights[x1][y1] < heights[x2][y2]:
+                return False
+            
+            visited.add((x2, y2))
+            return (canFlowAtlantic(x2, y2, x2 - 1, y2, visited) or
+                    canFlowAtlantic(x2, y2, x2, y2 - 1, visited) or
+                    canFlowAtlantic(x2, y2, x2 + 1, y2, visited) or
+                    canFlowAtlantic(x2, y2, x2, y2 + 1, visited))
+        
+        for i in range(r):
+            for j in range(c):
+                if (canFlowPacific(i, j, i, j, set()) and
+                    canFlowAtlantic(i, j, i, j, set())):
+                    answer.append([i, j])
+        
+        return answer
