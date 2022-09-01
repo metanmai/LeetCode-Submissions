f4d7c4ebdd268330882da64bdce4f10bef93b582
metanmai <103983719+metanmai@users.noreply.github.com>
2022-09-01 22:46:08 +0530

    Time: 45 ms (9.41%), Space: 38.8 MB (19.28%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> paths;
+    
     void hasPathSum(TreeNode* root, int targetSum,vector<int> res) {
-        if(not root)
+        if(!root)
             return;
+        
         TreeNode *curr = root;
         if(curr)
         {
             res.push_back(root->val);
-            if(curr->val == targetSum and not curr->left and not curr->right)
+            if(curr->val == targetSum and !curr->left and !curr->right) // Leaf Node.
                 paths.push_back(res);
             
             else
             {
-                hasPathSum(curr->left, targetSum - curr->val,res);
-                hasPathSum(curr->right, targetSum - curr->val,res);
+                hasPathSum(curr->left, targetSum - curr->val, res);
+                hasPathSum(curr->right, targetSum - curr->val, res);
             }
         }
     }
     
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         vector<int> res;
-        hasPathSum(root,targetSum,res);
+        hasPathSum(root, targetSum, res);
         return paths;
     }
 };
