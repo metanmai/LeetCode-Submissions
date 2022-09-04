c452c61b9783601c641587df8e021fce3fa4dbe0
metanmai <103983719+metanmai@users.noreply.github.com>
2022-09-04 19:45:06 +0530

    Time: 5211 ms (85.68%), Space: 14 MB (50.62%) - LeetHub

---
+++ 79-word-search/79-word-search.py
@@ -0,0 +1,32 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        r, c = len(board), len(board[0])
+        visited = set()
+        
+        def dfs(x, y, i):
+            if i == len(word):
+                return True
+            
+            if(x < 0 or y < 0 or
+               x >= r or y >= c or
+               board[x][y] != word[i] or
+               (x, y) in visited or board[x][y] != word[i]):
+                return False
+            
+            visited.add((x, y))
+            condition = (dfs(x + 1, y, i + 1) or
+                    dfs(x - 1, y, i + 1) or
+                    dfs(x, y + 1, i + 1) or
+                    dfs(x, y - 1, i + 1))
+            visited.remove((x, y))
+            return condition
+        
+        answer = False
+        for i in range(r):
+            for j in range(c):
+                if board[i][j] == word[0]:
+                    answer = dfs(i, j, 0)
+                    if answer:
+                        return True
+
+        return False
