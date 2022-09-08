9e6765c32e9ea6dea6d9196df60c44c9e9097105
metanmai <103983719+metanmai@users.noreply.github.com>
2022-09-08 10:06:05 +0530

    Time: 68 ms (6.14%), Space: 13.9 MB (79.14%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits == "":
+            return []
+        
+        maps = ["-", "-", "abc", "def","ghi","jkl","mno", "pqrs","tuv","wxyz"]
+        answer = []
+        
+        def combos(num, comb):
+            if len(comb) == len(digits):
+                answer.append(comb[ : ])
+                return
+            
+            for i in range(num, len(digits)):
+                for j in range(len(maps[int(digits[i])])):
+                    comb += maps[int(digits[i])][j]
+                    combos(i + 1, comb)
+                    comb = comb[ : -1]
+                    
+        combos(0, "")
+        return answer
