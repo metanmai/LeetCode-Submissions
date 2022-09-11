0f6f8f6a45867853df239ab9bfa8b17e95302755
metanmai <103983719+metanmai@users.noreply.github.com>
2022-09-11 19:17:40 +0530

    Time: 8 ms (76.60%), Space: 13.1 MB (59.56%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,20 +4,22 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         // Top row : Max profit in can't buy state, Bottom Row : Max profit in can buy state.
-        vector<vector<int>> maxProf(prices.size() + 1, vector<int> (2, -1));
+        vector<int> nextIndex(2, 0), currIndex(2, 0);
         
         // Filling the table.
-        maxProf[prices.size()][0] = maxProf[prices.size()][1] = 0;
         for(int startIndex = prices.size() - 1; startIndex >= 0; startIndex--)
+        {
             for(int buy = 0; buy <= 1; buy++)
                 if(buy)
-                    maxProf[startIndex][buy] = max(maxProf[startIndex + 1][0] - prices[startIndex], // Decided to buy.
-                                                   maxProf[startIndex + 1][1]); // Decided to not buy.
+                    currIndex[buy] = max(nextIndex[0] - prices[startIndex], // Decided to buy.
+                                         nextIndex[1]); // Decided to not buy.
 
                 else
-                    maxProf[startIndex][buy] = max(maxProf[startIndex + 1][1] + prices[startIndex], // Decided to sell.
-                                                   maxProf[startIndex + 1][0]); // Decided to not sell.
-        
-        return maxProf[0][1];
+                    currIndex[buy] = max(nextIndex[1] + prices[startIndex], // Decided to sell.
+                                                   nextIndex[0]); // Decided to not sell.
+            
+            nextIndex = currIndex;
+        }
+        return currIndex[1];
     }
 };
