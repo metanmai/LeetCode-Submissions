d684305d36f76cf304e60a3c6a259d8c02502115
metanmai <103983719+metanmai@users.noreply.github.com>
2022-09-11 16:37:24 +0530

    Time: 33 ms (5.34%), Space: 15.3 MB (20.04%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,26 +1,7 @@
-// Iteration + Tabulation Solution (Bottom-Up)
+// Iteration DP Solution (Bottom-Up)
 
 class Solution {
 public:
-    int profit(vector<int> prices, int startIndex, int buy, vector<vector<int>> &maxProf)
-    {
-        if(startIndex == prices.size())
-            return 0;
-        
-        if(maxProf[startIndex][buy] != -1)
-            return maxProf[startIndex][buy];
-        
-        if(buy)
-            maxProf[startIndex][buy] = max(profit(prices, startIndex + 1, 0, maxProf) - prices[startIndex], // Decided to buy.
-                                           profit(prices, startIndex + 1, 1, maxProf)); // Decided to not buy.
-
-        else
-            maxProf[startIndex][buy] = max(profit(prices, startIndex + 1, 1, maxProf) + prices[startIndex], // Decided to sell.
-                                           profit(prices, startIndex + 1, 0, maxProf)); // Decided to not sell.
-        
-        return maxProf[startIndex][buy];
-    }
-    
     int maxProfit(vector<int>& prices) {
         // Top row : Max profit in can't buy state, Bottom Row : Max profit in can buy state.
         vector<vector<int>> maxProf(prices.size() + 1, vector<int> (2, -1));
