35c5e8e13fbf84c1012944c94ef317c6a5287548
metanmai <103983719+metanmai@users.noreply.github.com>
2022-09-11 20:22:45 +0530

    Time: 352 ms (57.99%), Space: 75.3 MB (87.14%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,20 +1,23 @@
-// Interation + Tabulation Solution 2 (Bottom-Up)
+// Interative Solution 2 (Top-Down)
 
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> maxProfit(prices.size() + 1, vector<int> (5, 0));
-        int transactions = 4; // Counting buying and selling as seperate transactions, hence there are 4 transactions.
+        vector<int> currProfit(5, 0), nextProfit(5, 0);
         
         for(int startIndex = prices.size() - 1; startIndex >= 0; startIndex--)
+        {
+            // Counting buying and selling as seperate transactions, hence there are 4 transactions.
             for(int transactions = 1; transactions <= 4; transactions++)
                 if(transactions % 2) // You can only sell when the number of remaining transactions is odd and can only buy when it is even.
-                    maxProfit[startIndex][transactions] = max(maxProfit[startIndex + 1][transactions - 1] + prices[startIndex],
-                                                              maxProfit[startIndex + 1][transactions]);
+                    currProfit[transactions] = max(nextProfit[transactions - 1] + prices[startIndex],
+                                                   nextProfit[transactions]);
                 else
-                    maxProfit[startIndex][transactions] = max(maxProfit[startIndex + 1][transactions - 1] - prices[startIndex],
-                                                              maxProfit[startIndex + 1][transactions]);
-        
-        return maxProfit[0][4];
+                    currProfit[transactions] = max(nextProfit[transactions - 1] - prices[startIndex],
+                                                   nextProfit[transactions]);
+            
+            nextProfit = currProfit;
+        }
+        return currProfit[4];
     }
 };
