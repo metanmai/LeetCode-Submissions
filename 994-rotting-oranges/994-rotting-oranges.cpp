f70d728b680db24760777c2cf4357dd8ebefd668
metanmai <103983719+metanmai@users.noreply.github.com>
2022-09-24 14:54:04 +0530

    Time: 21 ms (15.45%), Space: 15.1 MB (5.09%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-    vector<vector<int>> directions = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-    
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        int freshCount = 0;
+        int time, timeMax = 0;
+        int freshCount = 0, rotCount = 0;
+        vector<vector<int>> directions = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
         queue<vector<int>> q;
-        vector<vector<int>> visited(m, vector<int> (n, 0));
         
         for(int i = 0; i < m; i++)
             for(int j = 0; j < n; j++)
@@ -15,13 +14,11 @@ public:
                     freshCount++;
                 
                 if(grid[i][j] == 2)
-                { 
                     q.push({i, j, 0}); // The first 2 elements are for the index and the last element keeps track of the level, a.k.a time.
-                    visited[i][j] = 2;
-                }
             }
         
-        int time, timeMax = 0, rotCount = 0;
+        
+        // BFS
         while(!q.empty())
         {
             auto front = q.front();
@@ -29,16 +26,16 @@ public:
             timeMax = max(time, timeMax);
             q.pop();
             
-            for(auto i : directions)
+            for(auto i : directions) // Process all the cells around current cell.
             {
                 int r = x + i[0], c = y + i[1];
                 if(r < 0 or r >= m or 
                    c < 0 or c >= n or 
-                   grid[r][c] != 1 or visited[r][c] == 2)
+                   grid[r][c] != 1)
                     continue;
 
                 q.push({r, c, time + 1});
-                visited[r][c] = 2;
+                grid[r][c] = 2;
                 rotCount++;
             }
         }
