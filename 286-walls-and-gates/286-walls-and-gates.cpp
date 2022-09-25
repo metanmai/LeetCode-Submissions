4fc6cafecb72ddc65872a260097912ccc629c110
metanmai <103983719+metanmai@users.noreply.github.com>
2022-09-25 19:35:34 +0530

    Time: 115 ms (25.19%), Space: 31.7 MB (15.38%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.cpp
+++ 286-walls-and-gates/286-walls-and-gates.cpp
@@ -4,14 +4,12 @@ public:
     
     void wallsAndGates(vector<vector<int>>& rooms) {
         int m = rooms.size(), n = rooms[0].size();
-        vector<vector<int>> visited(m, vector<int> (n, 0));
         queue<vector<int>> q;
         for(int i = 0; i < m; i++)
             for(int j = 0; j < n; j++)
                 if(rooms[i][j] == 0)
                 {
                     q.push({i, j, 0}); // Perform BFS from all the gates simultaneously. That way all rooms will always have min distance from the nearest gate.
-                    visited[i][j] = 1;
                 }
         
         while(!q.empty())
@@ -24,11 +22,9 @@ public:
                 int r = front[0] + dir[0], c = front[1] + dir[1], currMin = front[2];
                 if(r < 0 or c < 0 or 
                    r >= m or c >= n or
-                   visited[r][c] or 
-                   rooms[r][c] == 0 or rooms[r][c] == -1)
+                   rooms[r][c] != INT_MAX) // You don't need a visited array because if a room has been visited then it won't be INT_MAX.
                     continue;
             
-                visited[r][c] = 1;
                 rooms[r][c] = currMin + 1;
                 q.push({r, c, rooms[r][c]});
             }
