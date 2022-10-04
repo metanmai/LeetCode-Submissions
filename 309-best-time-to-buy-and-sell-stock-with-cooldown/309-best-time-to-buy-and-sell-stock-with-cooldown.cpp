ecdb648fa75a6763f60724decf523a04261aee79
metanmai <103983719+metanmai@users.noreply.github.com>
2022-10-04 18:13:39 +0530

    Time: 10 ms (43.62%), Space: 11.1 MB (91.37%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,24 +1,43 @@
 class Solution {
-public:   
+public:
+    int maxProf(vector<int> prices, int i, int canBuy)
+    {
+        if(i >= prices.size())
+            return 0;
+        
+        int profit = 0;
+        
+        if(canBuy)
+            profit = max(maxProf(prices, i + 1, 0) - prices[i],
+                         maxProf(prices, i + 1, 1));
+        
+        else
+            profit = max(maxProf(prices, i + 2, 1) + prices[i],
+                         maxProf(prices, i + 1, 0));
+        
+        return profit;
+    }
+    
     int maxProfit(vector<int>& prices) {
-        vector<vector<vector<int>>> profit(prices.size() + 1, vector<vector<int>> (2, vector<int> (2, 0)));
+        int n = prices.size();
+        vector<int> curr(2, 0), next(2, 0), nextnext(2, 0);
         
-        for(int i = prices.size() - 1; i >= 0; i--)
+        for(int i = n - 1; i >= 0; i--)
+        {
             for(int canBuy = 0; canBuy <= 1; canBuy++)
-                for(int wait = 0; wait <= 1; wait++)
-                {
-                    if(wait)
-                        profit[i][canBuy][wait] = profit[i + 1][1][0];
+            {
+                if(canBuy)
+                    curr[canBuy] = max(next[0] - prices[i],
+                                        next[1]);
 
-                    else if(canBuy) // This has to be else if and not just if because if wait it true then no other statement should get executed.
-                        profit[i][canBuy][wait] = max(profit[i + 1][0][0] - prices[i],
-                                                      profit[i + 1][1][0]);
-
-                    else
-                        profit[i][canBuy][wait] = max(profit[i + 1][1][1] + prices[i], // Wait becomes true only when you sell. 
-                                                      profit[i + 1][0][0]);
-                }
+                else
+                    curr[canBuy] = max(nextnext[1] + prices[i],
+                                        next[0]);
+            }
+            nextnext = next;
+            next = curr;
+        }
         
-        return profit[0][1][0];
+        return curr[1];
     }
 };
