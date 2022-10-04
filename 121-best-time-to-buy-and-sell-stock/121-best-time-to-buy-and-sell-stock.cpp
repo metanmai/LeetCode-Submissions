627cff3aef7e568f5f90197c5a6e7a2bbb38b07c
metanmai <103983719+metanmai@users.noreply.github.com>
2022-10-04 16:19:12 +0530

    Time: 284 ms (30.13%), Space: 93.4 MB (50.96%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minimumSoFar = 0, maxProfit = 0;
+        int maxProf = 0, minimumSoFar = 0;
         
-        for(int i = 1; i < prices.size(); i++)
+        for(int i = 0; i < prices.size(); i++)
+        {
             if(prices[i] < prices[minimumSoFar])
                 minimumSoFar = i;
-            else
-                maxProfit = max(maxProfit, prices[i] - prices[minimumSoFar]);
+            
+            maxProf = max(maxProf, prices[i] - prices[minimumSoFar]);
+        }
         
-        return maxProfit;
+        return maxProf;
     }
 };
