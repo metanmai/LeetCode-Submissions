b081e4bdc92e27d46eff2bc9dee732d9c73f9f0d
metanmai <103983719+metanmai@users.noreply.github.com>
2022-10-04 17:44:05 +0530

    Time: 1551 ms (13.28%), Space: 207 MB (39.44%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,23 +1,20 @@
-// Interative Solution 2 (Top-Down)
-
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<int> currProfit(5, 0), nextProfit(5, 0);
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>> (2, vector<int> (3, 0)));
         
-        for(int startIndex = prices.size() - 1; startIndex >= 0; startIndex--)
-        {
-            // Counting buying and selling as seperate transactions, hence there are 4 transactions.
-            for(int transactions = 1; transactions <= 4; transactions++)
-                if(transactions % 2) // You can only sell when the number of remaining transactions is odd and can only buy when it is even.
-                    currProfit[transactions] = max(nextProfit[transactions - 1] + prices[startIndex],
-                                                   nextProfit[transactions]);
-                else
-                    currProfit[transactions] = max(nextProfit[transactions - 1] - prices[startIndex],
-                                                   nextProfit[transactions]);
-            
-            nextProfit = currProfit;
-        }
-        return currProfit[4];
+        for(int i = n - 1; i >= 0; i--)
+            for(int canBuy = 0; canBuy <= 1; canBuy++)
+                for(int transactions = 1; transactions <= 2; transactions++)
+                    if(canBuy)
+                        dp[i][canBuy][transactions] = max(dp[i + 1][0][transactions] - prices[i],
+                                                          dp[i + 1][1][transactions]);
+
+                    else
+                        dp[i][canBuy][transactions] = max(dp[i + 1][1][transactions - 1] + prices[i],
+                                                          dp[i + 1][0][transactions]);
+        
+        return dp[0][1][2];
     }
 };
