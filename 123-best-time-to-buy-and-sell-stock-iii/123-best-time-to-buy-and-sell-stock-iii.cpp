12ef0681d89d5e1444b923a2935f4919dfb952c3
metanmai <103983719+metanmai@users.noreply.github.com>
2022-10-04 17:54:38 +0530

    Time: 459 ms (53.72%), Space: 75.1 MB (95.48%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>> (2, vector<int> (3, 0)));
+        vector<int> curr(5, 0), next(5, 0);
         
         for(int i = n - 1; i >= 0; i--)
-            for(int canBuy = 0; canBuy <= 1; canBuy++)
-                for(int transactions = 1; transactions <= 2; transactions++)
-                    if(canBuy)
-                        dp[i][canBuy][transactions] = max(dp[i + 1][0][transactions] - prices[i],
-                                                          dp[i + 1][1][transactions]);
+        {
+            for(int transactions = 1; transactions <= 4; transactions++)
+                if(transactions % 2 == 0)
+                    curr[transactions] = max(next[transactions - 1] - prices[i],
+                                             next[transactions]);
 
-                    else
-                        dp[i][canBuy][transactions] = max(dp[i + 1][1][transactions - 1] + prices[i],
-                                                          dp[i + 1][0][transactions]);
-        
-        return dp[0][1][2];
+                else
+                    curr[transactions] = max(next[transactions - 1] + prices[i],
+                                             next[transactions]);
+            
+            next = curr;
+        }
+        return curr[4];
     }
 };
