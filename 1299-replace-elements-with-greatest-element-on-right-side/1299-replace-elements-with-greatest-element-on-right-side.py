be9bee5feb9564cc774df86c7694317b5609e5c8
metanmai <103983719+metanmai@users.noreply.github.com>
2022-10-20 00:55:26 +0530

    Time: 294 ms (47.09%), Space: 15.1 MB (89.10%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -3,8 +3,7 @@ class Solution:
         maxi = -1
         
         for i in range(len(arr) - 1, -1, -1):
-            temp = arr[i]
-            arr[i] = maxi
+            temp, arr[i] = arr[i], maxi
             maxi = max(maxi, temp)
             
         return arr
