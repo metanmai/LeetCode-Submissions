6ffa78a8793bd3a7f65d4dbf17f4738dcea3155d
metanmai <103983719+metanmai@users.noreply.github.com>
2022-11-01 19:33:53 +0530

    Time: 1006 ms (5.04%), Space: 28.2 MB (29.24%) - LeetHub

---
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numPairsDivisibleBy60(vector<int>& time) {
+        sort(time.begin(), time.end());
+        
+        int n = time.size(), count = 0;
+        int maxi = *max_element(time.begin(), time.end());
+        
+        for(int i = 0; i < n - 1; i++)
+        {
+            int start = ceil(time[i] / 60.0) * 60 - time[i];
+            
+            for(int key = start; key <= maxi; key += 60)
+                if(binary_search(time.begin() + i + 1, time.end(), key))
+                    count += (upper_bound(time.begin() + i + 1, time.end(), key) -
+                              lower_bound(time.begin() + i + 1, time.end(), key));
+        }
+        
+        return count;
+    }
+};
