1971b04e5eda96dcaf5e3dd2d94da58dbc47fcaf
metanmai <103983719+metanmai@users.noreply.github.com>
2022-11-01 08:38:17 +0530

    Time: 368 ms (54.54%), Space: 138.3 MB (33.33%) - LeetHub

---
+++ 1858-longest-word-with-all-prefixes/1858-longest-word-with-all-prefixes.cpp
@@ -0,0 +1,61 @@
+struct Node
+{
+    Node *links[26];
+    bool flag = false;
+};
+
+class Trie {
+
+public:
+    Node *root;
+    
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node *node = root;
+        
+        for(char c : word)
+        {
+            if(!node->links[c - 'a'])
+                node->links[c - 'a'] = new Node();
+            
+            node = node->links[c - 'a'];
+        }
+        
+        node->flag = true;
+    }
+    
+    bool hasAllPrefixes(string prefix) {
+        Node *node = root;
+        
+        for(char c : prefix)
+        {
+            if(!node->links[c - 'a'] or !node->links[c - 'a']->flag)
+                return false;
+            
+            node = node->links[c - 'a'];
+        }
+        
+        return true;
+    }
+};
+
+class Solution {
+public:
+    string longestWord(vector<string>& words) {
+        sort(words.begin(), words.end()); // We are sorting so that longest is the lexicographically smallest.
+        Trie tr;
+        string longest = "";
+        
+        for(string word : words)
+            tr.insert(word);
+        
+        for(string word : words)
+            if(tr.hasAllPrefixes(word) and word.length() > longest.length())
+                longest = word;
+            
+        return longest;
+    }
+};
