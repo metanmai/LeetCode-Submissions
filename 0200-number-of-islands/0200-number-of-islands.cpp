55554a8e021b2c37e543ef0e4dc77de2e44e890a
metanmai <103983719+metanmai@users.noreply.github.com>
2022-11-04 06:29:16 +0530

    Time: 58 ms (75.13%), Space: 18.2 MB (22.84%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -5,7 +5,7 @@ public:
     vector<int> dirs = {0, 1, 0, -1, 0};
     int m, n;
     
-    void bfs(vector<vector<char>> grid, vector<vector<bool>> &visited, int x, int y)
+    void bfs(vector<vector<char>> &grid, int x, int y)
     {
         queue<pair<int, int>> q;
         q.push({x, y});
@@ -20,9 +20,9 @@ public:
                 int r = front.first + dirs[i], c = front.second + dirs[i + 1];
                 if(r >= 0 and c >= 0 and
                    r < m and c < n and 
-                   !visited[r][c] and grid[r][c] == '1')
+                   grid[r][c] == '1')
                 {
-                    visited[r][c] = true;
+                    grid[r][c] = '2';
                     q.push({r, c});
                 }
             }
@@ -32,19 +32,14 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         m = grid.size(), n = grid[0].size();
         int islands = 0;
-        vector<vector<bool>> visited(m, vector<bool> (n, false));
         
         for(int i = 0; i < m; i++)
-        {
             for(int j = 0; j < n; j++)
-            {
-                if(grid[i][j] == '1' and !visited[i][j])
+                if(grid[i][j] == '1')
                 {
-                    bfs(grid, visited, i, j);
+                    bfs(grid, i, j);
                     islands++;
                 }
-            }
-        }
         
         return islands;
     }
