106c0e141208a1d7272ce5331a03b16d303e68dc
metanmai <103983719+metanmai@users.noreply.github.com>
2022-11-06 03:21:41 +0530

    Time: 169 ms (5.03%), Space: 26.9 MB (5.03%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,18 +1,29 @@
-// DFS Solution.
+// BFS Solution.
 
 class Solution {
 public:
     vector<int> dirs = {0, 1, 0, -1, 0};
     int m, n;
     
-    void dfs(vector<vector<int>> &grid, int x, int y)
+    void bfs(vector<vector<int>> &grid, int x, int y)
     {
-        if(x >= 0 and y >= 0 and
-           x < m and y < n and grid[x][y] == 0)
+        queue<vector<int>> q;
+        q.push({x, y});
+        
+        while(!q.empty())
         {
-            grid[x][y] = 1;
-            for(int i = 0; i < 4; i++)
-                dfs(grid, x + dirs[i], y + dirs[i + 1]);
+            auto cell = q.front();
+            q.pop();
+            int r = cell[0], c = cell[1];
+            
+            if(r >= 0 and c >= 0 and
+               r < m and c < n and grid[r][c] == 0)
+            {
+                grid[r][c] = 1;
+                for(int i = 0; i < 4; i++)
+                    q.push({r + dirs[i], c + dirs[i + 1]});
+            }
+            
         }
     }
     
@@ -23,26 +34,26 @@ public:
         for(int i = 0; i < m; i++)
         {
             if(grid[i][0] == 0)
-                dfs(grid, i, 0);
+                bfs(grid, i, 0);
             
             if(grid[i][n - 1] == 0)
-                dfs(grid, i, n - 1);
+                bfs(grid, i, n - 1);
         }
         
         for(int j = 0; j < n; j++)
         {
             if(grid[0][j] == 0)
-                dfs(grid, 0, j);
+                bfs(grid, 0, j);
             
             if(grid[m - 1][j] == 0)
-                dfs(grid, m - 1, j);
+                bfs(grid, m - 1, j);
         }
         
         for(int i = 0; i < m; i++)
             for(int j = 0; j < n; j++)
                 if(grid[i][j] == 0)
                 {
-                    dfs(grid, i, j);
+                    bfs(grid, i, j);
                     islands++;
                 }
         return islands;
