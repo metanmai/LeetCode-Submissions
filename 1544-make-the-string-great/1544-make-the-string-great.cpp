217f09ceaee7fdb92519844e49e31d280e38b1a1
metanmai <103983719+metanmai@users.noreply.github.com>
2022-11-08 07:28:11 +0530

    Time: 10 ms (20.25%), Space: 6.2 MB (82.16%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        int i = 0, initial;
+        string answer;
+        
+        do
+        {
+            i = 0, initial = s.length();
+            answer = "";
+            
+            while(i < s.length())
+                if(i != s.length() - 1 and abs(s[i] - s[i + 1]) == 32)
+                    i += 2;
+
+                else
+                    answer.push_back(s[i++]);
+            
+            s = answer;
+        }
+        while(s.length() != initial);
+        
+        return s;
+    }
+};
