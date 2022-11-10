d3c325f74720744409431b395da44f93d62a23d7
metanmai <103983719+metanmai@users.noreply.github.com>
2022-11-10 10:58:47 +0530

    Time: 546 ms (24.23%), Space: 9.3 MB (43.91%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> squares;
+        vector<int> minSquares(n + 1, n);
+        minSquares[0] = 0;
+        int sq = 1;
+        
+        while(sq * sq <= n)
+        {
+            squares.push_back(sq * sq);
+            minSquares[sq * sq] = 1;
+            sq++;
+        }
+        
+        for(int i = 1; i <= n; i++)
+            for(int sq : squares)
+                if(i > sq)
+                    minSquares[i] = min(minSquares[i], minSquares[i - sq] + 1);
+        
+        return minSquares[n];
+    }
+};
