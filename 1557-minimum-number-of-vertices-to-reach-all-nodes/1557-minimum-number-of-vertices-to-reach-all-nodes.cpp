d84f814100dcf370a61f6bc49cd7261dc435fe82
metanmai <103983719+metanmai@users.noreply.github.com>
2022-11-16 21:59:55 +0530

    Time: 349 ms (88.81%), Space: 106.7 MB (36.77%) - LeetHub

---
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        /* 
+        Just return all the nodes which have indegree = 0, because :
+        - Nodes with indegree 0 cannot be reached from any other node, so they must be in the answer.
+        - Every other node can be reached from some other node. 
+        */
+        
+        vector<int> indegree(n), answer;
+        
+        for(auto edge : edges)
+            indegree[edge[1]]++;
+        
+        for(int i = 0; i < n; i++)
+            if(!indegree[i])
+                answer.push_back(i);
+        
+        return answer;
+    }
+};
