5c0f9899536d97c52fca2b509d9a0fef71d59db6
metanmai <103983719+metanmai@users.noreply.github.com>
2022-11-26 18:20:26 +0530

    Time: 203 ms (23.53%), Space: 32.2 MB (43.69%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,46 @@
+class Solution {
+private:
+    struct cell
+    {
+        int x, y;
+    };
+    
+    vector<int> dirs = {0, 1, 0, -1, 0};
+    
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<bool>> visited(m, vector<bool> (n));
+        queue<cell> q;
+        
+        for(int i = 0; i < m; i++)
+            for(int j = 0; j < n; j++)
+                if(mat[i][j] == 0)
+                {
+                    q.push({i, j});
+                    visited[i][j] = true;
+                }
+        
+        while(!q.empty())
+        {
+            cell curr = q.front();
+            q.pop();
+            
+            for(int i = 0; i < 4; i++)
+            {
+                int nextRow = curr.x + dirs[i], nextCol = curr.y + dirs[i + 1];
+                
+                if(nextRow >= 0 and nextCol >= 0 and
+                   nextRow < m and nextCol < n and
+                   mat[nextRow][nextCol] != 0 and !visited[nextRow][nextCol])
+                {
+                    visited[nextRow][nextCol] = true;
+                    mat[nextRow][nextCol] = mat[curr.x][curr.y] + 1;
+                    q.push({nextRow, nextCol});
+                }
+            }
+        }
+        
+        return mat;
+    }
+};
