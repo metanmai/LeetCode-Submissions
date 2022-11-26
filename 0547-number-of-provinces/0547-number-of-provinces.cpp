f0f22b77d04c8d5d077af59c4ef41159b3aee989
metanmai <103983719+metanmai@users.noreply.github.com>
2022-11-26 11:06:11 +0530

    Time: 71 ms (12.99%), Space: 50 MB (5.11%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,6 +1,6 @@
 class Solution {
-public:
-    int find(vector<int> &parent, int node)
+private:
+    int findParent(vector<int> parent, int node)
     {
         while(parent[node] != node)
             node = parent[node];
@@ -8,38 +8,40 @@ public:
         return node;
     }
     
-    int unionize(vector<int> &parent, vector<int> &rank, int node1, int node2)
+    int performUnion(vector<int> &parent, vector<int> &rank, int node1, int node2)
     {
-        int parent1 = find(parent, node1), parent2 = find(parent, node2);
+        int parent1 = findParent(parent, node1), parent2 = findParent(parent, node2);
+        
         if(parent1 == parent2)
             return 0;
         
-        if(rank[parent1] > rank[parent2])
+        if(rank[node1] > rank[node2])
         {
             parent[parent2] = parent1;
-            rank[parent1] += rank[parent2];
+            rank[node1] += rank[node2];
         }
         
         else
         {
-            parent[parent1]  = parent2;
-            rank[parent2] += rank[parent1];
+            parent[parent1] = parent2;
+            rank[node2] += rank[node1];
         }
         
         return 1;
     }
     
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size(), provinces = n;
-        vector<int> parent, rank(n, 1);
+        vector<int> parent(n), rank(n, 1);
         
         for(int i = 0; i < n; i++)
-            parent.push_back(i);
+            parent[i] = i;
         
         for(int i = 0; i < n; i++)
             for(int j = 0; j < n; j++)
                 if(isConnected[i][j])
-                    provinces -= unionize(parent, rank, i, j);
+                    provinces -= performUnion(parent, rank, i, j);
         
         return provinces;
     }
