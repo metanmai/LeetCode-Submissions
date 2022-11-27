9387d24fa674501c90e1b9208f6c777842147aae
metanmai <103983719+metanmai@users.noreply.github.com>
2022-11-27 13:57:19 +0530

    Time: 28 ms (84.17%), Space: 15 MB (19.34%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indegree(numCourses);
+        map<int, vector<int>> adj;
+        queue<int> q;
+        int topoLen = 0;
+        
+        for(auto edge : prerequisites)
+        {
+            indegree[edge[1]]++;
+            adj[edge[0]].push_back(edge[1]);
+        }
+        
+        for(int i = 0; i < numCourses; i++)
+            if(indegree[i] == 0)
+                q.push(i);
+        
+        while(!q.empty())
+        {
+            int curr = q.front();
+            q.pop();
+            
+            topoLen++;
+            
+            for(int node : adj[curr])
+            {
+                indegree[node]--;
+                
+                if(indegree[node] == 0)
+                    q.push(node);
+            }
+        }
+        
+        return topoLen == numCourses;
+    }
+};
