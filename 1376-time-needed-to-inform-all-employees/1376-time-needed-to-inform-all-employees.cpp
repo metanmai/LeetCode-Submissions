e7e9023f73574ac7d3e2b7fde88cb53ec2053fc1
metanmai <103983719+metanmai@users.noreply.github.com>
2022-12-01 21:32:53 +0530

    Time: 1093 ms (11.62%), Space: 176.2 MB (5.09%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        int minTime = 0;
+        vector<vector<int>> adj(n);
+        queue<vector<int>> q; // {node, time}.
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(manager[i] != -1)
+                adj[manager[i]].push_back(i);
+        }
+        
+        q.push({headID, 0});
+        
+        while(!q.empty())
+        {
+            auto curr = q.front();
+            q.pop();
+
+            int currNode = curr[0], currTime = curr[1];
+            for(auto node : adj[currNode])
+                q.push({node, currTime + informTime[currNode]});
+            
+            minTime = max(minTime, currTime);
+        }
+        
+        return minTime;
+    }
+};
