20a1735422d37274a14dafeba5140a31b50cb601
metanmai <103983719+metanmai@users.noreply.github.com>
2022-12-03 05:06:42 +0530

    Time: 265 ms (24.66%), Space: 27 MB (30.07%) - LeetHub

---
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) {
+        vector<int> dirs = {jug1Capacity, -jug1Capacity, jug2Capacity, -jug2Capacity};
+        vector<bool> visited(jug1Capacity + jug2Capacity + 1);
+        queue<int> q;
+        q.push(0);
+        
+        while(!q.empty())
+        {
+            int curr = q.front();
+            q.pop();
+            
+            if(curr < 0 or curr > jug1Capacity + jug2Capacity or
+               visited[curr])
+                continue;
+            
+            visited[curr] = true;
+            
+            if(curr == targetCapacity)
+                return true;
+            
+            for(int i = 0; i < 4; i++)
+                q.push(curr + dirs[i]);
+        }
+        
+        return false;
+    }
+};
