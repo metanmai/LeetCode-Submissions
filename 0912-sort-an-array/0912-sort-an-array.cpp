89e6e1f32d1bcc4dbd549f1268141b0f6b7c9956
Tanmai Niranjan <metanmai@icloud.com>
2023-03-01 10:50:14 +0530

    Time: 1196 ms (7.83%), Space: 307.1 MB (5.51%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,58 @@
+class Solution {
+private:
+    template<typename T>
+    vector<T> mergeSort(vector<T> &arr, int l, int r)
+    {
+        if(l < r)
+        {
+            int mid = (l + r) / 2;
+            vector<T> leftArr = mergeSort(arr, l, mid);
+            vector<T> rightArr = mergeSort(arr, mid + 1, r);
+            
+            return mergeArrays(leftArr, rightArr);
+        }
+        
+        return {arr[l]};
+    }
+    
+    template<typename T>
+    vector<T> mergeArrays(const vector<T> &arr1, const vector<T> &arr2)
+    {
+        vector<T> combined;
+        auto it1 = arr1.begin(), it2 = arr2.begin();
+        
+        while(it1 != arr1.end() and it2 != arr2.end())
+        {
+            if(*it1 < *it2)
+            {
+                combined.push_back(*it1);
+                it1++;
+            }
+            
+            else
+            {
+                combined.push_back(*it2);
+                it2++;
+            }
+        }
+        
+        while(it1 != arr1.end())
+        {
+            combined.push_back(*it1);
+            it1++;
+        }
+        
+        while(it2 != arr2.end())
+        {
+            combined.push_back(*it2);
+            it2++;
+        }
+        
+        return combined;
+    }
+    
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        return mergeSort<int>(nums, 0, nums.size() - 1);
+    }
+};
