43371ac075176be62203ebfe7a82ccb3fc092b33
Tanmai Niranjan <metanmai@icloud.com>
2023-03-17 10:27:58 +0530

    Time: 60 ms (84.54%), Space: 175.3 MB (58.76%) - LeetHub

---
+++ 1490-clone-n-ary-tree/1490-clone-n-ary-tree.cpp
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneTree(Node* root) {
+        if(!root)
+            return NULL;
+        
+        Node *cloneRoot = new Node(root->val);
+        
+        for(auto child : root->children)
+            cloneRoot->children.push_back(cloneTree(child));
+        
+        return cloneRoot;
+    }
+};
