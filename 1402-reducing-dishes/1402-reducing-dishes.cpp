6edf7a0d27c3241a9c1fb2fb5d42cc969bdec69e
Tanmai Niranjan <metanmai@icloud.com>
2023-03-29 11:11:42 +0530

    Time: 7 ms (85.02%), Space: 8 MB (98.26%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,28 +1,19 @@
 class Solution {
-private:
-    int n;
-    
-    int findMaxSatisfaction(vector<int> &satisfaction, vector<vector<int>> &dp, int i, int time)
-    {
-        if(i == n)
-            return 0;
-        
-        if(dp[i][time] != -1)
-            return dp[i][time];
-        
-        int take = satisfaction[i] * time + findMaxSatisfaction(satisfaction, dp, i + 1, time + 1);
-        int notTake = findMaxSatisfaction(satisfaction, dp, i + 1, time);
-        
-        return dp[i][time] = max(take, notTake);
-        
-    }
-    
 public:
     int maxSatisfaction(vector<int>& satisfaction) {
-        n = satisfaction.size();
+        int n = satisfaction.size(), curr = 0, total = 0;
         sort(satisfaction.begin(), satisfaction.end());
-        vector<vector<int>> dp(n + 1, vector<int> (n + 1, -1));
         
-        return findMaxSatisfaction(satisfaction, dp, 0, 1);
+        for(int i = n - 1; i >= 0; i--)
+        {
+            curr += satisfaction[i];
+            
+            if(curr < 0)
+                break;
+            
+            total += curr;
+        }
+        
+        return total;
     }
 };
