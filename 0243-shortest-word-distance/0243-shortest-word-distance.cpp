08ebe5614e741ee1a87ac1ed498a0d58a2344160
Tanmai Niranjan <metanmai@icloud.com>
2023-04-07 12:29:02 +0530

    Time: 13 ms (36.19%), Space: 11.7 MB (58.73%) - LeetHub

---
+++ 0243-shortest-word-distance/0243-shortest-word-distance.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int shortestDistance(vector<string>& wordsDict, string word1, string word2) {
+        int minDist = INT_MAX, prevWord = 0, prevIndex = -1;
+        vector<int> wordIndices;
+        
+        for(int i = 0; i < wordsDict.size(); i++)
+        {
+            if(wordsDict[i] == word1 and prevWord == 2 or
+               wordsDict[i] == word2 and prevWord == 1)
+                minDist = min(minDist, i - prevIndex);
+               
+            if(wordsDict[i] == word1 or wordsDict[i] == word2)
+            {
+                if(wordsDict[i] == word1)
+                    prevWord = 1;
+                
+                else
+                    prevWord = 2;
+                
+                prevIndex = i;
+            }
+               
+        }
+        
+        return minDist;
+    }
+};
