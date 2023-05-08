14850ac42567767c14a07d733acdd3ab90bf6866
Tanmai Niranjan <metanmai@icloud.com>
2023-05-08 15:19:42 +0530

    Time: 29 ms (6.30%), Space: 8.3 MB (13.75%) - LeetHub

---
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> multiply(vector<vector<int>>& mat1, vector<vector<int>>& mat2) {
+        int m = mat1.size(), n = mat1[0].size(), p = mat2[0].size();
+        vector<vector<int>> answer(m, vector<int> (p));
+        
+        for(int i = 0; i < m; i++)
+            for(int j = 0; j < p; j++)
+                for(int k = 0; k < n; k++)
+                    answer[i][j] += mat1[i][k] * mat2[k][j];
+        
+        
+        return answer;
+    }
+};
