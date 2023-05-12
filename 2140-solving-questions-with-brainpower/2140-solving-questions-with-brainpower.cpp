61d1103d2ce4071f61379279109e985b50350369
Tanmai Niranjan <metanmai@icloud.com>
2023-05-12 16:46:39 +0530

    Time: 391 ms (35.51%), Space: 115.3 MB (84.05%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,29 +1,22 @@
 #define ll long long
 
 class Solution {
-private:
-    ll findMaxPoints(vector<vector<int>> &questions, vector<ll> &dp, 
-                     int pos, int n)
-    {
-        if(pos >= questions.size())
-            return 0;
-        
-        if(dp[pos] != -1)
-            return dp[pos];
-        
-        ll take = (questions[pos][0] + 
-                    findMaxPoints(questions, dp, pos + questions[pos][1] + 1, n));
-                    
-        ll notTake = findMaxPoints(questions, dp, pos + 1, n);
-        
-        return dp[pos] = max(take, notTake);        
-    }
-    
 public:
     ll mostPoints(vector<vector<int>>& questions) {
         int n = questions.size();
-        vector<ll> dp(n, -1);
+        vector<ll> dp(n + 1);
         
-        return findMaxPoints(questions, dp, 0, n);
+        for(int pos = n - 1; pos >= 0; pos--)
+        {
+            ll take = questions[pos][0];
+            if(pos + questions[pos][1] + 1 < n)
+                take += dp[pos + questions[pos][1] + 1];
+            
+            ll notTake = dp[pos + 1];
+            
+            dp[pos] = max(take, notTake);
+        }
+        
+        return dp[0];
     }
 };
