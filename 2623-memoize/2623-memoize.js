282398f7b160d5010f3764880ca92c2db631c831
Tanmai Niranjan <metanmai@icloud.com>
2023-05-13 10:28:49 +0530

    Time: 341 ms (86.23%), Space: 108.8 MB (5.86%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+     hash = {}
+    
+    return function(...args) {
+        var key = JSON.stringify(args);
+        
+            if(key in hash)
+            return hash[key];
+            
+            hash[key] = fn(...args);
+            return hash[key];
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
