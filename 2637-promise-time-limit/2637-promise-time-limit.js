4a700b373b373ec8b733911e95f2312aa94937ca
Tanmai Niranjan <metanmai@icloud.com>
2023-05-17 12:53:18 +0530

    Time: 55 ms (84.03%), Space: 41.7 MB (85.93%) - LeetHub

--- 2637-promise-time-limit/2637-promise-time-limit.js
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -4,13 +4,20 @@
  * @return {Function}
  */
 var timeLimit = function(fn, t) {
-  return async function(...args) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        reject("Time Limit Exceeded");
-      }, t);
-      fn(...args).then(resolve).catch(reject);
-    })
+    return async function(...args) {
+        return new 
+        Promise((resolve, reject) => 
+        {
+            setTimeout(() => 
+            {
+                reject("Time Limit Exceeded")
+            }, t);
+            
+            fn(...args)
+                .then(resolve)
+                .catch(reject)
+                .finally(() => clearTimeout());
+        })
   }
 };
 
