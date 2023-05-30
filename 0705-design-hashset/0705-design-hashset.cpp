3826d1dd11e9e8d52a3b292650b2b465acedf8c3
Tanmai Niranjan <metanmai@icloud.com>
2023-05-30 21:34:27 +0530

    Time: 95 ms (71.88%), Space: 50.2 MB (29.45%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/design-hashset/discuss/773006/C%2B%2B-3-Approaches
+
+class MyHashSet {
+private:
+	int prime;
+	vector<list<int>> table;
+
+	int hash(int key) {
+		return key % prime;
+	}
+
+	list<int>::iterator search(int key) {
+		int h = hash(key);
+		return find(table[h].begin(), table[h].end(), key);
+	}
+
+public:
+	MyHashSet() : prime(10007), table(prime) {}
+	
+	void add(int key) {
+		int h = hash(key);
+		if (!contains(key))
+			table[h].push_back(key);
+	}
+	
+	void remove(int key) {
+		int h = hash(key);
+		auto it = search(key);
+		if (it != table[h].end())
+			table[h].erase(it);
+	}
+	
+	bool contains(int key) {
+		int h = hash(key);
+		return search(key) != table[h].end();
+	}
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
