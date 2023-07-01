17b4ff9cb7cfbad2c0c79af572454cc54cb7785b
Tanmai Niranjan <metanmai@icloud.com>
2023-07-01 19:10:51 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (29.20%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,29 +1,16 @@
 class Solution {
-private:
-    int findDecodings(vector<int> &dp, string &s, int i, int n)
-    {
-        if(i == n)
-            return 1;
-        
-        if(s[i] == '0')
-            return 0;
-        
-        if(dp[i] != -1)
-            return dp[i];
-        
-        int pick = findDecodings(dp, s, i + 1, n), notPick = 0;
-        
-        if(i + 1 < n and stoi(s.substr(i, 2)) <= 26)
-            notPick = findDecodings(dp, s, i + 2, n);
-        
-        return dp[i] = pick + notPick;
-    }
-    
 public:
     int numDecodings(string s) {
         int n = s.length();
-        vector<int> dp(n, -1);
+        vector<int> dp(n + 1);
         
-        return findDecodings(dp, s, 0, n);
+        dp[n] = 1;
+        
+        for(int i = n - 1; i >= 0; i--)
+            if(s[i] != '0')
+                dp[i] = dp[i + 1] + ((i + 1 < n and 
+                                      stoi(s.substr(i, 2)) <= 26) ? dp[i + 2] : 0);
+        
+        return dp[0];
     }
 };
