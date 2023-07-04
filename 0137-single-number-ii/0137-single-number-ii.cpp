4d1c81de19f25a7618d92f08c2e534b75d9ffd95
Tanmai Niranjan <metanmai@icloud.com>
2023-07-04 23:34:41 +0530

    Time: 4 ms (90.46%), Space: 9.4 MB (72.62%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int seenOnce = 0, seenTwice = 0;
+
+        for (int num : nums) 
+        {
+            seenOnce = (seenOnce ^ num) & (~seenTwice);
+            seenTwice = (seenTwice ^ num) & (~seenOnce);
+        }
+        
+        return seenOnce;
+    }
+};
