9eaba99b097dcc3476dbb0175f1c7109f293a7af
Tanmai Niranjan <metanmai@icloud.com>
2023-07-12 07:16:56 +0530

    Time: 1121 ms (5.07%), Space: 135.4 MB (10.92%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,35 +1,30 @@
 class Solution {
-private:
-    int findBestScore(vector<vector<int>> &dp, vector<vector<int>> &players, 
-                      int prev, int i, int n)
-    {
-        if(i == n)
-            return 0;
-        
-        if(dp[prev + 1][i] != -1)
-            return dp[prev + 1][i];
-        
-        int take = 0, notTake = 0;
-        
-        if(prev == -1 or players[i][1] >= players[prev][1])
-            take = players[i][1] + findBestScore(dp, players, i, i + 1, n);
-        
-        notTake = findBestScore(dp, players, prev, i + 1, n);
-        
-        return dp[prev + 1][i] = max(take, notTake);
-    }
-    
 public:
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
         int n = scores.size();
-        vector<vector<int>> players(n), dp(n, vector<int> (n, -1));
+        vector<vector<int>> players(n), dp(n + 1, vector<int> (n + 1));
         
         for(int i = 0; i < n; i++)
             players[i] = {ages[i], scores[i]};
         
         sort(players.begin(), players.end());
         
-        return findBestScore(dp, players, -1, 0, n);
+        for(int i = n - 1; i >= 0; i--)
+        {
+            for(int prev = n - 1; prev >= -1; prev--)
+            {
+                int take = 0, notTake = 0;
+        
+                if(prev == -1 or players[i][1] >= players[prev][1])
+                    take = players[i][1] + dp[i + 1][i + 1];
+
+                notTake = dp[prev + 1][i + 1];
+                
+                dp[prev + 1][i] = max(take, notTake);
+            }
+        }
+        
+        return dp[0][0];
     }
     
 };
