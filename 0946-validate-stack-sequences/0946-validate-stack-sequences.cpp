d0565d62eabbf70b73a26a0bf0da9193d774fe52
Tanmai Niranjan <metanmai@icloud.com>
2023-07-14 17:09:25 +0530

    Time: 3 ms (98.93%), Space: 15.3 MB (50.81%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int n = pushed.size(), j = 0;
         stack<int> st;
-        int n = pushed.size();
-        int j = 0;
         
         for(int i = 0; i < n; i++)
         {
             st.push(pushed[i]);
             
             while(!st.empty() and st.top() == popped[j])
-            {
-                st.pop();
-                j++;
-            }
+                st.pop(), j++;
         }
         
         return st.empty();
