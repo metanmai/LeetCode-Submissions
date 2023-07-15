088469b41810a0402db22d291e2a68600cc82298
Tanmai Niranjan <metanmai@icloud.com>
2023-07-15 09:10:04 +0530

    Time: 182 ms (66.58%), Space: 54.4 MB (77.24%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -3,7 +3,6 @@ class Solution {
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         int n = startTime.size();
         vector<int> dp(n + 1);
-        
         vector<vector<int>> jobs(n);
         
         for(int i = 0; i < n; i++)
@@ -14,7 +13,8 @@ class Solution {
         
         for(int i = n - 1; i >= 0; i--)
         {
-            int nextPos = lower_bound(startTime.begin(), startTime.end(), jobs[i][1]) - startTime.begin();
+            int nextPos = (lower_bound(startTime.begin(), startTime.end(), jobs[i][1]) - 
+                           startTime.begin());
         
             int take = jobs[i][2] + dp[nextPos];
             int notTake = dp[i + 1];
