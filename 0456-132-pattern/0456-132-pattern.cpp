4cd54a764a536747b0edb6c98c72120a270754d6
Tanmai Niranjan <metanmai@icloud.com>
2023-07-16 19:49:03 +0530

    Time: 82 ms (60.39%), Space: 38.6 MB (23.15%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int n = nums.size();
+        
+        if (n < 3)
+            return false;
+        
+        stack<int> stk;
+        vector<int> min_array(n);
+        min_array[0] = nums[0];
+
+        for (int i = 1; i < n; i++) 
+            min_array[i] = min(min_array[i - 1], nums[i]);
+        
+        for (int j = n - 1; j > 0; j--) 
+        {
+            if (nums[j] <= min_array[j])
+                continue;
+            
+            while (!stk.empty() and stk.top() <= min_array[j]) 
+                stk.pop();
+            
+            if (!stk.empty() and stk.top() < nums[j])
+                return true;
+            
+            stk.push(nums[j]);
+        }
+        
+        return false;
+    }
+};
