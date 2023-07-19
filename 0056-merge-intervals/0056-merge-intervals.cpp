32601d27a19acd1a29de0b2a6ab71d80f52017f5
Tanmai Niranjan <metanmai@icloud.com>
2023-07-19 09:40:22 +0530

    Time: 34 ms (95.10%), Space: 19.1 MB (46.18%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = intervals.size();
+        int n = intervals.size(), k = 0;
         sort(intervals.begin(), intervals.end());
-        vector<vector<int>> answer;
+        vector<vector<int>> answer = {intervals[0]};
         
-        for(int i = 0; i < n - 1; i++)
+        for(int i = 1; i < n; i++)
         {
-            if(intervals[i][1] >= intervals[i + 1][0]) // merge
-            {
-                intervals[i + 1][0] = intervals[i][0];
-                intervals[i + 1][1] = max(intervals[i][1], intervals[i + 1][1]);
-            }
+            if(intervals[i][0] <= answer[k][1])
+                answer[k][1] = max(intervals[i][1], answer[k][1]);
             
             else
+            {
                 answer.push_back(intervals[i]);
+                k++;
+            }
         }
         
-        answer.push_back(intervals.back());
         return answer;
     }
 };
