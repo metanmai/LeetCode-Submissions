3e66353a59363c6df30f7003d85c75b994c65e04
Tanmai Niranjan <metanmai@icloud.com>
2023-07-22 16:52:11 +0530

    Time: 254 ms (13.57%), Space: 36.5 MB (12.99%) - LeetHub

---
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    vector<vector<int>> dirs = {{2, 1}, {2, -1}, {-2, 1}, {-2, -1},
+                                {1, 2}, {1, -2}, {-1, 2}, {-1, -2}};
+    
+    double findProbability(vector<vector<vector<double>>> &dp, int row, int col, int k, int n)
+    {
+        if(!(min(row, col) >= 0 and max(row, col) < n))
+            return 0;
+        
+        if(k == 0)
+            return 1;
+        
+        // This base case actually made more sense to me, but idk.
+        
+        // if(k == 0)
+        //     return (double) (min(row, col) >= 0 and max(row, col) < n);
+        
+        if(dp[row][col][k] != -1)
+            return dp[row][col][k];
+        
+        double prob = 0;
+        
+        for(auto dir : dirs)
+            prob += findProbability(dp, row + dir[0], col + dir[1], k - 1, n);
+        
+        return dp[row][col][k] = prob / 8;
+    }
+    
+public:
+    double knightProbability(int n, int k, int row, int column) {
+        vector<vector<vector<double>>> dp(n, vector<vector<double>> (n, vector<double> (k + 1, -1)));
+        return findProbability(dp, row, column, k, n);
+    }
+};
