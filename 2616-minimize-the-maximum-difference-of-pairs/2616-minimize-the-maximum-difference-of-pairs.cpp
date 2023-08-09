021fc2e1f92e9b32550b62df5ce4446955f22c54
Tanmai Niranjan <metanmai@icloud.com>
2023-08-09 09:34:34 +0530

    Time: 134 ms (91.40%), Space: 81.3 MB (15.35%) - LeetHub

---
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minimizeMax(vector<int>& nums, int p) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int low = 0, high = nums.back() - nums.front();
+        
+        auto isValidPair = [&] (int diff) -> bool
+        {
+            int i = 1;
+            int pairs = 0;
+            
+            while(i < n and pairs < p)
+            {
+                if(nums[i] - nums[i - 1] <= diff)
+                    pairs++, i++;
+                
+                i++;
+            }
+            
+            return pairs >= p;
+        };
+        
+        while(low < high)
+        {
+            int mid = (high + low) / 2;
+            
+            if(isValidPair(mid))
+                high = mid;
+            
+            else
+                low = mid + 1;
+        }
+        
+        return high;
+    }
+};
