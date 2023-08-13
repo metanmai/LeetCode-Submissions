fae35ad90f5921147e1d52ed36363fd8bb090df1
Tanmai Niranjan <metanmai@icloud.com>
2023-08-13 16:24:09 +0530

    Time: 16 ms (90.48%), Space: 16.8 MB (44.44%) - LeetHub

---
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    int minCostPalindrome(vector<vector<int>> &dp, string &s, int i, int j, int n)
+    {
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        while(i < j)
+        {
+            if(s[i] != s[j])
+                return (dp[i][j] = 1 + min(
+                minCostPalindrome(dp, s, i + 1, j, n),
+                minCostPalindrome(dp, s, i, j - 1, n)
+                ));
+            
+            else
+                i++, j--;
+        }
+        
+        return 0;
+    }
+    
+public:
+    bool isValidPalindrome(string s, int k) {
+        int n = s.length();
+        vector<vector<int>> dp(n + 1, vector<int> (n + 1, -1));
+        
+        return minCostPalindrome(dp, s, 0, n - 1, n) <= k;
+    }
+};
