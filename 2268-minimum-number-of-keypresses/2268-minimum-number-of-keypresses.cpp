eaeeb0c70b999c5512951a884ef7d64df10fdc5d
Tanmai Niranjan <metanmai@icloud.com>
2023-08-13 23:04:41 +0530

    Time: 17 ms (44.24%), Space: 9 MB (72.12%) - LeetHub

--- 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.cpp
+++ 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minimumKeypresses(string s) {
-        int answer = 0;
+        int answer = 0, pos = 1, num = 1;;
         vector<int> letters(26);
         
         for(char ch : s)
@@ -9,12 +9,9 @@ public:
         
         sort(letters.begin(), letters.end(), greater<> ());
         
-        int pos = 1, num = 1;
-        
         for(auto ele : letters)
         {
             answer += pos * ele;
-            
             num++;
             
             if(num == 10)
