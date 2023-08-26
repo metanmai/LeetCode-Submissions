007740d706c95893be955c124af9fc0fccd19bd7
Tanmai Niranjan <metanmai@icloud.com>
2023-08-26 11:16:52 +0530

    Time: 0 ms (100.00%), Space: 7 MB (90.83%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -1,30 +1,25 @@
-// Time: O(n^2)
-// Space: O(n)
+// DP Approach
+
+// Time: O(n)
+// Space: O(1)
 
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int> (2, 1)); // 0 for down, 1 for up.
+        if(nums.size() == 1)
+            return 1;
         
-        for(int i = n - 1; i >= 0; i--)
-            for(int j = i + 1; j < n; j++)
-            {
-                if(nums[i] == nums[j])
-                     continue;
-                
-                bool dir = nums[i] < nums[j];
-                
-                dp[i][dir] = max(dp[i][dir], 1 + dp[j][!dir]);
-            }
+        int n = nums.size(), up = 1, down = 1;
         
-        auto mySort = [&] (vector<int> &a, vector<int> &b) -> bool 
+        for(int i = 1; i < n; i++)
         {
-            return max(a[0], a[1]) < max(b[0], b[1]);
-        };
+            if(nums[i] > nums[i - 1])
+                up = down + 1;
+            
+            else if(nums[i] < nums[i - 1])
+                down = up + 1;
+        }
         
-        auto maxChain = *max_element(dp.begin(), dp.end(), mySort);
-        
-        return *max_element(maxChain.begin(), maxChain.end());
+        return max(up, down);
     }
 };
