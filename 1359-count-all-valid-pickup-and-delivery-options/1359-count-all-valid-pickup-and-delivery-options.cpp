a2acc68bd4bf8851f06e0a4ca21afbf5b57b58cf
Tanmai Niranjan <metanmai@icloud.com>
2023-09-10 17:18:23 +0530

    Time: 2 ms (56.67%), Space: 6 MB (71.30%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,34 +1,20 @@
-// Brother man this is somewhat like parentheses. DP Solution.
-// Time Complexity: O(n^2)
-// Space Complexity: O(n^2)
+/* 
+This math solution is neat. Why didn't you think of this?
+Ways to arrange pickups is your usual n!, so just multiply with i on each step.
+Ways to arrange deliveries follows the pattern: 1, 3, 5, 7, ... 2n - 1. 
+*/
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 
 class Solution {
-private:
-    int MOD = 1e9 + 7;
-    using lolo = long long;
-    
-    lolo totalWays(vector<vector<int>> &dp, int unpicked, int undelivered) {
-        if (unpicked == 0 and undelivered == 0)
-            return 1;
-        
-        if (unpicked < 0 or undelivered < 0 or undelivered < unpicked)
-            return 0;
-        
-        if (dp[unpicked][undelivered] != -1)
-            return dp[unpicked][undelivered];
-        
-        lolo ways = 0;
-        
-        ways = (ways + (unpicked * totalWays(dp, unpicked - 1, undelivered))) % MOD;
-        ways = (ways + ((undelivered - unpicked) * totalWays(dp, unpicked, undelivered - 1))) % MOD;
-        
-        return dp[unpicked][undelivered] = ways;
-    }
-    
 public:
     int countOrders(int n) {
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        int MOD = 1e9 + 7;
+        long long answer = 1;
         
-        return totalWays(dp, n, n);
+        for(int i = 1; i <= n; i++)
+            answer = ((answer % MOD) * ((i * (2 * i - 1)) % MOD)) % MOD;
+        
+        return answer;
     }
 };
