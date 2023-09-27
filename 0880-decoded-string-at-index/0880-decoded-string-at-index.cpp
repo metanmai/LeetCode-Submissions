9b981d39b0a27243c4cdd41a964ebb28f935945f
Tanmai Niranjan <metanmai@icloud.com>
2023-09-28 00:02:47 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (11.71%) - LeetHub

--- 0880-decoded-string-at-index/0880-decoded-string-at-index.cpp
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.cpp
@@ -5,7 +5,7 @@ public:
         long long len = 0;
 
         // Find length of decoded string
-        for (int i = 0; i < n; ++i)
+        for (int i = 0; i < n; i++)
             len = (s[i] >= '1' and s[i] <= '9') ? len * (s[i] - '0') : len + 1;
 
         for (int i = n - 1; i >= 0; i--) 
