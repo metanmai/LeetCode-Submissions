03ebd102ebabd28ea9b8f0bd1cc5d8745cb2e7a5
Tanmai Niranjan <metanmai@icloud.com>
2023-09-27 23:57:38 +0530

    Time: 2 ms (45.12%), Space: 6.6 MB (7.16%) - LeetHub

---
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string decodeAtIndex(string s, int k) {
+        int n = s.length();
+        long long len = 0;
+
+        // Find length of decoded string
+        for (int i = 0; i < n; ++i)
+            len = (s[i] >= '1' and s[i] <= '9') ? len * (s[i] - '0') : len + 1;
+
+        
+        for (int i = n - 1; i >= 0; i--) 
+        {
+            k %= len;
+            
+            if (k == 0 and isalpha(s[i]))
+                return (string) "" + s[i];
+
+            if (isdigit(s[i]))
+                len /= s[i] - '0';
+            else
+                len--;
+        }
+        return "";
+    }
+};
