062ebf3efb798a6b8e77f20504d2b8670f391118
Tanmai Niranjan <metanmai@icloud.com>
2023-09-28 20:16:18 +0530

    Time: 12 ms (49.43%), Space: 20.8 MB (11.56%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,5 +1,5 @@
 /* 
-Prefix/Suffix Max Solution
+Stack Solution
 Time Complexity: O(n)
 Space Complexity: O(n)
 */
@@ -8,16 +8,25 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size(), totalWater = 0;
-        vector<int> prefixMax(n, height.front()), suffixMax(n, height.back());
-        
-        for(int i = 1; i < n; i++)
-            prefixMax[i] = max(prefixMax[i - 1], height[i]);
-        
-        for(int i = n - 2; i >= 0; i--)
-            suffixMax[i] = max(suffixMax[i + 1], height[i]);
+        stack<int> st;
         
         for(int i = 0; i < n; i++)
-            totalWater += min(prefixMax[i], suffixMax[i]) - height[i];
+        {
+            while (!st.empty() and height[i] > height[st.top()]) 
+            {
+                int curr = st.top(); st.pop();
+                
+                if (st.empty())
+                    break;
+                
+                int distance = i - st.top() - 1;
+                int level = min(height[i], height[st.top()]) - height[curr];
+                
+                totalWater += distance * level;
+            }
+            
+            st.push(i);
+        }
         
         return totalWater;
     }
