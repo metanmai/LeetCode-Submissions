21ffc5ca8814115598401e807a15631a73e38458
Tanmai Niranjan <metanmai@icloud.com>
2023-09-29 13:37:17 +0530

    Time: 10 ms (11.09%), Space: 14.5 MB (6.63%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,6 +1,43 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        return find(nums.begin(), nums.end(), target) != nums.end();
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        
+        while(low <= high)
+        {
+            int mid = midpoint(low, high);
+            
+            if(nums[mid] == target)
+                return true;
+            
+            if (nums[low] == nums[mid])
+            {
+                low++; 
+                continue;
+            }
+            
+            bool pivotArray = nums[low] < nums[mid], targetArray = nums[low] <= target;
+            
+            if (pivotArray ^ targetArray)
+            {
+                if (pivotArray)
+                    low = mid + 1;
+                
+                else
+                    high = mid - 1;
+            } 
+            
+            else
+            {
+                if (nums[mid] < target)
+                    low = mid + 1;
+                
+                else
+                    high = mid - 1;
+            }
+        }
+        
+        return false;
     }
 };
