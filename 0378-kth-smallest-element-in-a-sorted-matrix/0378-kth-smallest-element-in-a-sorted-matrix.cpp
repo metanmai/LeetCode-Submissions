e0157b08f4eb9f39a7c7855bcc0248d882f582c8
Tanmai Niranjan <metanmai@icloud.com>
2023-09-29 16:36:49 +0530

    Time: 25 ms (66.27%), Space: 13.3 MB (71.36%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,25 +1,42 @@
 /*
-Min-Heap Solution
-Time Complexity: O(klog k)
-Space Complexity: O(k)
+Binary Search Solution
+Time Complexity: O(nlog(max - min))
+Space Complexity: O(log n)
 */
 
 class Solution {
+private:
+    int countLessOrEqual(vector<vector<int>>& matrix, int x, int n) 
+    {
+        int cnt = 0, c = n - 1;
+        
+        for (int r = 0; r < n; r++) 
+        {
+            while(c >= 0 and matrix[r][c] > x) 
+                c--;
+            
+            cnt += c + 1;
+        }
+        
+        return cnt;
+    }
+    
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int n = matrix.size(), answer;
-        priority_queue<vector<int>, vector<vector<int>>, greater<>> minHeap;
-        for (int r = 0; r < min(n, k); ++r)
-            minHeap.push({matrix[r][0], r, 0});
-
-        for (int i = 1; i <= k; ++i) {
-            auto top = minHeap.top(); minHeap.pop();
-            int r = top[1], c = top[2];
-            answer = top[0];
+        int n = matrix.size();
+        int low = matrix[0][0], high = matrix[n - 1][n - 1], answer = -1;
+        
+        while (low <= high) 
+        {
+            int mid = midpoint(low, high);
             
-            if (c + 1 < n) 
-                minHeap.push({matrix[r][c + 1], r, c + 1});
+            if (countLessOrEqual(matrix, mid, n) >= k) 
+                answer = mid, high = mid - 1;
+            
+            else 
+                low = mid + 1;
         }
+        
         return answer;
     }
 };
