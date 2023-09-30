753127d653eb5394cc67373aedb7449a42847a6c
Tanmai Niranjan <metanmai@icloud.com>
2023-09-30 13:30:00 +0530

    Time: 65 ms (71.49%), Space: 50.7 MB (11.42%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -12,15 +12,15 @@ public:
         vector<int> prefixMin(n); // This is for the '1' in 1, 3, 2.
         prefixMin[0] = nums[0];
 
-        for (int i = 1; i < n; i++) 
+        for(int i = 1; i < n; i++) 
             prefixMin[i] = min(prefixMin[i - 1], nums[i]);
         
-        for (int j = n - 1; j > 0; j--) // This is for the '2' in 1, 3, 2.
+        for(int j = n - 1; j > 0; j--) // This is for the '2' in 1, 3, 2.
         {
-            while (!st.empty() and st.top() <= prefixMin[j]) 
+            while(!st.empty() and st.top() <= prefixMin[j]) 
                 st.pop();
             
-            if (!st.empty() and st.top() < nums[j])
+            if(!st.empty() and st.top() < nums[j])
                 return true;
             
             st.push(nums[j]);
