3cdbd5ba456aaa27cd101910f2b64a4f483640c9
Tanmai Niranjan <metanmai@icloud.com>
2023-09-30 12:16:13 +0530

    Time: 7 ms (62.59%), Space: 8.8 MB (13.24%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -1,33 +1,39 @@
+#define MAXI numeric_limits<int>::max()
+/*
+Top-Down Partition Solution.
+Time Complexity: O(n^3)
+Space Complexity: O(n^2)
+*/
+
 class Solution {
 private:
-    int solve(vector<vector<int>> &dp, vector<int> &values, int i, int j)
+    int findMinScore(vector<vector<int>> &dp, vector<int> &values, int i, int j)
     {
-        if(i == j)
+        if(j - i == 1)
             return 0;
-
+        
         if(dp[i][j] != -1)
             return dp[i][j];
-
-        int mini = INT_MAX;
-
-        for(int k = i; k < j; k++)
+        
+        int minScore = MAXI;
+        
+        for(int k = i + 1; k < j; k++)
         {
-            int tempAns = (
-                solve(dp, values, i, k) + solve(dp, values, k + 1, j) + 
-                values[i - 1] * values[k] * values[j]
-            );
-            mini = min(mini, tempAns);
-        }  
-
-        return dp[i][j] = mini;      
+            int currScore = values[i] * values[k] * values[j];
+            int leftScore = findMinScore(dp, values, i, k);
+            int rightScore = findMinScore(dp, values, k, j);
+            
+            minScore = min(minScore, currScore + leftScore + rightScore);
+        }
+        
+        return dp[i][j] = minScore;
     }
     
 public:
-    int minScoreTriangulation(vector<int>& values) 
-    {
+    int minScoreTriangulation(vector<int>& values) {
         int n = values.size();
         vector<vector<int>> dp(n, vector<int> (n, -1));
         
-        return solve(dp, values, 1, n - 1);
+        return findMinScore(dp, values, 0, values.size() - 1);
     }
 };
