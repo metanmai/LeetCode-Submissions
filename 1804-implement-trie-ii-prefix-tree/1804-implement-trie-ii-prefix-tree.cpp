5d5618020aab10189f7b01288d68e35fa411da99
Tanmai Niranjan <metanmai@icloud.com>
2023-10-01 12:50:26 +0530

    Time: 134 ms (100.00%), Space: 65.5 MB (52.69%) - LeetHub

--- 1804-implement-trie-ii-prefix-tree/1804-implement-trie-ii-prefix-tree.cpp
+++ 1804-implement-trie-ii-prefix-tree/1804-implement-trie-ii-prefix-tree.cpp
@@ -1,35 +1,35 @@
 struct Node
 {
     Node *links[26];
-    int count1 = 0, count2 = 0;
+    int prefixCount = 0, wordCount = 0;
 };
 
 class Trie {
 public:
     Node *root;
-    
+
     Trie() {
         root = new Node();
     }
     
     void insert(string word) {
         Node *node = root;
-        
+
         for(char c : word)
         {
             if(!node->links[c - 'a'])
                 node->links[c - 'a'] = new Node();
             
-            node->links[c - 'a']->count2++;
             node = node->links[c - 'a'];
+            node->prefixCount++;
         }
-        
-        node->count1++;
+
+        node->wordCount++;
     }
     
     int countWordsEqualTo(string word) {
         Node *node = root;
-        
+
         for(char c : word)
         {
             if(!node->links[c - 'a'])
@@ -37,34 +37,34 @@ public:
             
             node = node->links[c - 'a'];
         }
-        
-        return node->count1;
+
+        return node->wordCount;
     }
     
     int countWordsStartingWith(string prefix) {
         Node *node = root;
-        
+
         for(char c : prefix)
         {
-            if(!node->links[c - 'a'] or !node->links[c - 'a'])
+            if(!node->links[c - 'a'])
                 return 0;
             
             node = node->links[c - 'a'];
         }
-        
-        return node->count2;
+
+        return node->prefixCount;
     }
     
     void erase(string word) {
         Node *node = root;
-        
+
         for(char c : word)
         {
-            node->links[c - 'a']->count2--;
             node = node->links[c - 'a'];
+            node->prefixCount--;
         }
-        
-        node->count1--;
+
+        node->wordCount--;
     }
 };
 
