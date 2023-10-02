86beb450b421525865c0768c44289ed3321e70d6
Tanmai Niranjan <metanmai@icloud.com>
2023-10-02 13:48:05 +0530

    Time: 195 ms (50.00%), Space: 60.9 MB (21.43%) - LeetHub

--- 2093-minimum-cost-to-reach-city-with-discounts/2093-minimum-cost-to-reach-city-with-discounts.cpp
+++ 2093-minimum-cost-to-reach-city-with-discounts/2093-minimum-cost-to-reach-city-with-discounts.cpp
@@ -1,9 +1,13 @@
+/* 
+Do not use PQ here, because there can be some ele in the Queue that might not be the shortest one now, but is the shortest one later.
+*/
+
 class Solution {
 public:
     int minimumCost(int n, vector<vector<int>>& highways, int discounts) {
         vector<vector<pair<int, int>>> adj(n);
         vector<int> dist(n, INT_MAX);
-        queue<vector<int>> pq;
+        queue<vector<int>> q;
         dist[0] = 0;
         
         for(auto edge : highways)
@@ -12,11 +16,11 @@ public:
             adj[edge[1]].push_back({edge[0], edge[2]});
         }
         
-        pq.push({0, 0, discounts}); // {distance, node, discounts}.
+        q.push({0, 0, discounts}); // {distance, node, discounts}.
         
-        while(!pq.empty())
+        while(!q.empty())
         {
-            auto curr = pq.front(); pq.pop();
+            auto curr = q.front(); q.pop();
             int currDist = curr[0], currNode = curr[1], currDisc = curr[2];
             
             for(auto [nextNode, nextDist] : adj[currNode])
@@ -24,10 +28,10 @@ public:
                 int lastDist = dist[nextNode];
                 
                 if(currDist + nextDist < lastDist)
-                    pq.push({dist[nextNode] = currDist + nextDist, nextNode, currDisc});
+                    q.push({dist[nextNode] = currDist + nextDist, nextNode, currDisc});
                 
                 if(currDisc > 0 and currDist + (nextDist / 2) < lastDist)
-                    pq.push({dist[nextNode] = currDist + (nextDist / 2), nextNode, currDisc - 1});
+                    q.push({dist[nextNode] = currDist + (nextDist / 2), nextNode, currDisc - 1});
             }
         }
         
